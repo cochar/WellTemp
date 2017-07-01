dd5ad312d7c3298790b9f3be1b985cf2829a8fcf
WellTemp <13269561368@163.com>
2017-07-01 15:21:03 +0800

    binding.

---
+++ socialprac/src/com/well/socialprac/module/BindingModule.java
@@ -0,0 +1,14 @@
+package com.well.socialprac.module;
+
+import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.Ok;
+
+@At("/binding")
+public class BindingModule extends BaseModule {
+
+	@At("/token")
+	@Ok("json")
+	public String getToken(String id){
+		return "success";
+	}
+}
